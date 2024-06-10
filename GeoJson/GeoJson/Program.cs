33ed6564a98f6405d3c3a646dbfb999973660a25
may0n3z <maksn19092006@gmail.com>
2024-06-10 12:18:37 +0300

    Add files via upload

---
+++ GeoJson/GeoJson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GeoJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Student\Desktop\ИСП-8\GeoJson";
+
+            var geoJsonData = GeoJSONLibrary.LoadGeoJSON(filePath);
+
+            var geometryType = GeoJSONLibrary.GetGeometryType(geoJsonData);
+            Console.WriteLine("Тип геометрии: " + geometryType);
+
+            var coordinates = GeoJSONLibrary.GetCoordinates(geoJsonData);
+            Console.WriteLine("Координаты: " +  coordinates);
+
+            var area = GeoJSONLibrary.CalculateArea(geoJsonData);
+            Console.WriteLine("Площадь: " + area);
+
+            var nearestFeature = GeoJSONLibrary.CalculateDistance(geoJsonData, new double[] { 0, 0 });
+            Console.WriteLine("Ближайший объект: " + nearestFeature);
+
+            string outputfilePath = @"C:\Users\Student\Desktop\ИСП-8\GeoJson";
+            GeoJSONLibrary.SaveGeoJSON(outputfilePath, geoJsonData);
+            Console.WriteLine("GeoJson сохранен в: " + outputfilePath);
+        }
+    }
+}
---
+++ GeoJson/GeoJson1/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GeoJson
+{
+    public class GeoJSONData
+    {
+        public string Type { get; set; }
+        public JToken Geometry { get; set; }
+    }
+
+    public class GeoJSONLibrary
+    {
+        public static GeoJSONData LoadGeoJSON(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<GeoJSONData>(json);
+        }
+
+        public static void SaveGeoJSON(string filePath, GeoJSONData data)
+        {
+            string json = JsonConvert.SerializeObject(data);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static string GetGeometryType(GeoJSONData data)
+        {
+            return data.Geometry["type"].ToString();
+        }
+
+        public static List<double> GetCoordinates(GeoJSONData data)
+        {
+            if (data.Geometry["type"].ToString() == "Point")
+            {
+                return data.Geometry["coordinates"].ToObject<List<double>>();
+            }
+            else if (data.Geometry["type"].ToString() == "LineString" || data.Geometry["type"].ToString() == "Polygon")
+            {
+                return data.Geometry["coordinates"].First().ToObject<List<double>>();
+            }
+            else
+            {
+                throw new NotImplementedException("Не реализовано для данного типа геометрии.");
+            }
+        }
+
+        public static double CalculateArea(GeoJSONData data)
+        {
+            if (data.Geometry["type"].ToString() == "Polygon")
+            {
+                // Реализация расчета площади полигона
+                // Используйте координаты из data.Geometry["coordinates"]
+                // Например, с помощью алгоритма Шеннона
+                throw new NotImplementedException("Реализация расчета площади полигона не завершена.");
+            }
+            else
+            {
+                throw new ArgumentException("Геометрия не является полигоном.");
+            }
+        }
+
+        public static double CalculateDistance(GeoJSONData data1, GeoJSONData data2)
+        {
+            if (data1.Geometry["type"].ToString() == "Point" && data2.Geometry["type"].ToString() == "Point")
+            {
+                // Реализация расчета расстояния между двумя точками
+                // Используйте координаты из data1.Geometry["coordinates"] и data2.Geometry["coordinates"]
+                // Например, с помощью формулы Гаверсина
+                throw new NotImplementedException("Реализация расчета расстояния между точками не завершена.");
+            }
+            else
+            {
+                throw new ArgumentException("Геометрии должны быть точками.");
+            }
+        }
+
+        public static object CalculateDistance(GeoJSONData geoJsonData, double[] doubles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
